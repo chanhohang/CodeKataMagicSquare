5dda965db15103530764948a7f20a6164069892c
ullink <ullink@7-rchanhohang2.ullink.lan>
2014-04-02 14:42:59 +0800

    solution for odd number and 4M magic Square

--- java/src/main/MagicSquare.java
+++ java/src/main/MagicSquare.java
@@ -1,4 +1,59 @@
 
 public class MagicSquare {
 
+	public int[][] findMagicSquare(int size)
+	{
+		int[][] result = null;
+		if (size <=2)
+		{
+			result = new int[][]{};
+		}
+		// Odd 
+		else if ((size & 1) == 1)
+		{
+			result = new int[size][size];
+			for (int i = 1 ; i <= size; i++)
+			{
+				for (int j = 1; j <= size; j++)
+				{
+					result[i-1][j-1] = size * ((i+j-1+size/2) % size) + ((i+2*j-2) % size) + 1;
+				}
+			}
+		}
+		// 4m
+		else if (size % 4 == 0)
+		{
+			result = new int[size][size];
+			for (int row=0;row<size;row++)
+			{
+				for (int column=0;column< size;column++)
+				{
+					result[row][column]= row*size+column+1;
+				}
+			}
+			
+			/*
+			 * switch square[n][p] and square[size-n][size-p]
+			 */
+			for (int row = 0; row < size; row ++)
+			{
+				for (int column = 0 ; column < size/2 ; column++)
+				{
+					if (!(row %4 == column %4) && !( (size-row-1)%4 == column%4)) 
+					{
+						int temp = result[row][column];
+						result[row][column] = result[size-1-row][size-1-column];
+						result[size-1-row][size-1-column] = temp;
+					}
+				}
+			}
+		}
+		// 4M + 2
+		else
+		{
+			
+		}
+			
+		return result;
+	}
 }
--- java/src/test/TestMagicSquare.java
+++ java/src/test/TestMagicSquare.java
@@ -1,4 +1,84 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
 public class TestMagicSquare {
 
+	private int size;
+
+	public TestMagicSquare(int size) {
+		this.size = size;
+	}
+
+	@Parameters
+	public static Collection<Object[]> init() {
+		List<Object[]> list = new ArrayList<Object[]>();
+		for (int i = 3; i < Short.MAX_VALUE / 10; i++)
+		{
+			if (i % 4 != 2)
+			{
+				list.add(new Integer[] {i});
+			}
+		}
+		return list;
+//		return Arrays.asList(new Object[][] { 
+//				{ 3 }, { 5 }, { 7 },
+//				{4}, {8}, {12},
+//		
+//		});
+	}
+
+	@Test
+	public void test() {
+		MagicSquare square = new MagicSquare();
+
+
+		long sum = (long) size *  (long) ((long) size * (long) size + 1) / 2;
+		int[][] squareResult = square.findMagicSquare(size);
+		printMagicSquare(squareResult);
+		for (int i = 0; i < size; i++) {
+			int result = 0;
+			for (int j = 0; j < size; j++) {
+				result += squareResult[i][j];
+			}
+			Assert.assertEquals("magic Square size=" + size + "|row="+ i, sum, result);
+
+		}
+		for (int i = 0; i < size; i++) {
+			int result = 0;
+			for (int j = 0; j < size; j++) {
+				result += squareResult[j][i];
+			}
+			Assert.assertEquals("magic Square size=" + size + "|col="+ i, sum, result);
+		}
+		int result = 0;
+		for (int i = 0; i < size; i++) {
+			result += squareResult[i][i];
+		}
+		Assert.assertEquals(sum, result);
+
+		// backslash
+		result = 0;
+		for (int i = 0; i < size; i++) {
+			result += squareResult[size - 1 - i][i];
+		}
+		Assert.assertEquals(sum, result);
+
+		
+	}
+
+	private void printMagicSquare(int[][] squareResult) {
+//		for (int i = 0; i < size; i++)
+//		{
+//			System.out.println(Arrays.toString(squareResult[i]));
+//		}
+	}
 }
